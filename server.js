bc08f2e458fd69e72312f8e33e740b9eb7c8964c
JongDeokRyu <deok8851@gmail.com>
2022-09-20 21:54:36 +0900

    Study bootstrap installation and server transfer through get and post

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
     console.log('listening on 8080')
@@ -7,4 +9,17 @@ app.listen(8080, function () {
 
 app.get('/pet', function (요청, 응답) {
     응답.send('펫용품 사시오')
+})
+
+app.get('/', function (요청, 응답) {
+    응답.sendFile(__dirname + '/index.html')
+})
+
+app.get('/write', function (요청, 응답) {
+    응답.sendFile(__dirname + '/write.html')
+})
+
+app.post('/add', function (요청, 응답) {
+    응답.send('전송완료')
+    console.log(요청.body);
 })
