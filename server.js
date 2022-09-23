cf8e23e4f9e79f0d2a0a3805f71cf89182263f2f
JongDeokRyu <deok8851@gmail.com>
2022-09-23 22:15:07 +0900

    install mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const MongoClient = require('mongodb').MongoClient
+
+
+var db;
+MongoClient.connect('mongodb+srv://admin:dkdkeld159@cluster0.ukzyigk.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
+    if (에러) return console.log(에러)
+    db = client.db('todoapp');
+
+
+
+    app.listen(8080, function () {
+        console.log('listening on 8080')
+    });
+});
+
 app.listen(8080, function () {
     console.log('listening on 8080')
 })
