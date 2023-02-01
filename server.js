ec74d4025df5865c681d76e44324801d9dc5da18
JongDeokRyu <deok8851@gmail.com>
2023-02-01 21:25:22 +0900

    상세 페이지 만들기 그리고 Bootstrap 디자인 넣기

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
 
+app.use("/public", express.static("public"));
+
 var db;
 MongoClient.connect(
   "mongodb+srv://admin:dkdkeld159@cluster0.ukzyigk.mongodb.net/?retryWrites=true&w=majority",
@@ -30,11 +32,11 @@ MongoClient.connect(
 );
 
 app.get("/", function (요청, 응답) {
-  응답.sendFile(__dirname + "/index.html");
+  응답.render("index.ejs");
 });
 
 app.get("/write", function (요청, 응답) {
-  응답.sendFile(__dirname + "/write.html");
+  응답.render("write.ejs");
 });
 
 app.post("/add", function (요청, 응답) {
@@ -84,4 +86,12 @@ app.delete("/delete", function (요청, 응답) {
     }
   });
   // .find()
+});
+
+app.get("/detail/:id", function (요청, 응답) {
+  요청.params.id = parseInt(요청.params.id);
+  db.collection("post").findOne({ _id: 요청.params.id }, function (에러, 결과) {
+    console.log(결과);
+    응답.render("detail.ejs", { data: 결과 });
+  });
 });
