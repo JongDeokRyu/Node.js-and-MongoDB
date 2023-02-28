7cbbc0c18b2db831db8a3d27ecb2e2ec47bc2b1a
JongDeokRyu <deok8851@gmail.com>
2023-02-28 22:47:23 +0900

    회원가입 기능 추가

--- server.js
+++ server.js
@@ -18,18 +18,11 @@ MongoClient.connect(
     if (에러) return;
     {
       console.log(에러);
-      console.log("에러다 !!!!!!!!!!!!!!");
     }
     db = client.db("todoapp");
 
-    db.collection("post").insertOne(
-      { 이름: "Deok", _id: 101 },
-      function (에러, 결과) {
-        console.log("저장완료");
-      }
-    );
     app.listen(8080, function () {
-      console.log("listening on 8080 ㅋㅋㅋㅋㅋㅋ");
+      console.log("DB 접속");
     });
   }
 );
@@ -42,32 +35,6 @@ app.get("/write", function (요청, 응답) {
   응답.render("write.ejs");
 });
 
-app.post("/add", function (요청, 응답) {
-  응답.send("전송완료");
-  db.collection("counter").findOne(
-    { name: "게시물갯수" },
-    function (에러, 결과) {
-      console.log("게시물 갯수 결과", 결과.totalPost);
-      var 총개시물갯수 = 결과.totalPost;
-
-      db.collection("post").insertOne(
-        { _id: 총개시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
-        function () {
-          console.log("저장완료");
-          db.collection("counter").updateOne(
-            { name: "게시물갯수" },
-            { $inc: { totalPost: 1 } },
-            function (에러, 결과) {}
-          );
-          if (에러) {
-            return console.log(에러);
-          }
-        }
-      );
-    }
-  );
-});
-
 app.get("/list", function (요청, 응답) {
   db.collection("post")
     .find()
@@ -75,20 +42,6 @@ app.get("/list", function (요청, 응답) {
       console.log(결과);
       응답.render("list.ejs", { posts: 결과 });
     });
-});
-
-app.delete("/delete", function (요청, 응답) {
-  console.log(요청.body);
-  요청.body._id = parseInt(요청.body._id);
-  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
-    console.log("삭제완료");
-    응답.status(200).send({ message: "status 200 성공했습니다." });
-    if (결과) {
-      console.log("결과는 ? ");
-      console.log(결과);
-    }
-  });
-  // .find()
 });
 
 app.get("/detail/:id", function (요청, 응답) {
@@ -220,7 +173,64 @@ app.get("/search", (요청, 응답) => {
   db.collection("post")
     .aggregate(검색조건)
     .toArray((에러, 결과) => {
-      console.log("sdfsd", 결과);
       응답.render("search.ejs", { posts: 결과 });
     });
+});
+
+app.post("/register", (요청, 응답) => {
+  console.log("*** 요청 *** ---> ", 요청.body);
+  db.collection("login").insertOne(
+    { id: 요청.body.id, pw: 요청.body.pw },
+    (에러, 결과) => {
+      응답.redirect("/");
+    }
+  );
+});
+
+app.post("/add", function (요청, 응답) {
+  응답.send("전송완료");
+  console.log("*** 요청 *** ---> ", 요청);
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
+    function (에러, 결과) {
+      console.log("게시물 갯수 결과", 결과.totalPost);
+      var 총개시물갯수 = 결과.totalPost;
+
+      var 저장할거 = {
+        _id: 총개시물갯수 + 1,
+        제목: 요청.body.title,
+        날짜: 요청.body.date,
+        작성자: 요청.user._id,
+      };
+
+      db.collection("post").insertOne(저장할거, function () {
+        console.log("저장완료");
+        db.collection("counter").updateOne(
+          { name: "게시물갯수" },
+          { $inc: { totalPost: 1 } },
+          function (에러, 결과) {}
+        );
+        if (에러) {
+          return console.log(에러);
+        }
+      });
+    }
+  );
+});
+
+app.delete("/delete", function (요청, 응답) {
+  console.log(요청.body);
+  요청.body._id = parseInt(요청.body._id);
+
+  var 삭제할데이터 = { _id: 요청.body._id, 작성자: 요청.user._id };
+
+  db.collection("post").deleteOne(삭제할데이터, function (에러, 결과) {
+    console.log("삭제완료");
+    응답.status(200).send({ message: "status 200 성공했습니다." });
+    if (결과) {
+      console.log("결과는 ? ");
+      console.log(결과);
+    }
+    if (에러) console.log("*** 에러 *** ---> ", 에러);
+  });
 });
